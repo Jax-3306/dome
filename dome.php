b403714eadf40e301cadb8bb33cb116e760f0bfb
Jax-3306 <jax9458846586@gmail.com>
2021-06-22 16:29:23 +0800

    dome.php加了much方法

--- dome.php
+++ dome.php
@@ -5,9 +5,18 @@ function show()
 {
     echo "这是一个什么东西啊";
 }
-function get(){
+
+function get()
+{
     echo "这是一个get方法";
 }
-function post(){
+
+function post()
+{
     echo "这是一个post方法";
+}
+
+function mach()
+{
+    echo "这是mach方法";
 }
